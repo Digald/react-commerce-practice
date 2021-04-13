56547707cb616b95feddb4c7702d9363252fbb3c
Mark Alaniz <markalaniz000@gmail.com>
2021-04-13 17:11:26 -0500

    Create shop page and styles

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import './App.css';
-import HomePage from './pages/homepage/homepage.component.jsx';
 import { Switch, Route } from 'react-router-dom';
-
-const HatsPage = () => (
-  <div>
-    <h1>hats page</h1>
-  </div>
-);
+import HomePage from './pages/homepage/homepage.component.jsx';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/hats" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );

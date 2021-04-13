103aa292f2268179ade234c1c74a31bbf776090e
Mark Alaniz <markalaniz000@gmail.com>
2021-04-13 18:43:52 -0500

    sign in form ui complete

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+import SignInPage from './pages/signinpage/signinpage.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SignInPage} />
       </Switch>
     </div>
   );

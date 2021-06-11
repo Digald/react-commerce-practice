6ca5905b2fa704a52041d71bc8ad6593e03fee33
Mark Alaniz <markalaniz000@gmail.com>
2021-06-11 16:15:30 -0500

    Finish section 11, collection routing

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import { Route } from "react-router-dom";
+import CollectionPage from "../collection/collection.component";
 
-const ShopPage = () => {
+const ShopPage = ({ match }) => {
+  console.log("match", match);
   return (
     <div classname="shop-page">
-      <CollectionsOverview />
+      <Route
+        exact
+        path={`${match.path}`}
+        component={CollectionsOverview}
+      ></Route>
+      <Route
+        path={`${match.path}/:collectionId`}
+        component={CollectionPage}
+      ></Route>
     </div>
   );
 };

f328423ac779de48c4a39ae23b64f1da5495417d
Mark Alaniz <markalaniz000@gmail.com>
2021-06-14 16:49:52 -0500

    typo

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -35,7 +35,7 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className="test-warning">
       *Please use the following test credit card for payments*
       <br></br>
-      4242 4242 4242 4242 - Exp: 08/24 - CVV:123
+      4242 4242 4242 4242 - Exp: 08/24 - CVV: 123
     </div>
     <StripeCheckoutButton price={total} />
   </div>

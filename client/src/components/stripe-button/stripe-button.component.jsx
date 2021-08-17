ec5fe4c0e36809524a33030de3c7887a7581b98c
Mark Alaniz <markalaniz000@gmail.com>
2021-08-16 20:12:01 -0500

    Add stripe payment api endpoint. Finish section 22

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
@@ -7,8 +8,22 @@ const StripeCheckoutButton = ({ price }) => {
     "pk_test_51J2MBZJkZ8XgVKmgzP7pENrWQoO72NXETJE52n3DwLTnyvax9UEkEmMUOBdG9JyNhdWmWBoT5P2zfzXnNOdBPvUQ00urQjrBga";
 
   const onToken = (token) => {
-    console.log(token);
-    alert("Payment Successful");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert("Payment Successful");
+      })
+      .catch((error) => {
+        alert(
+          "There was an issue with your payment, Please ensure you use the provided credit card."
+        );
+      });
   };
 
   return (
